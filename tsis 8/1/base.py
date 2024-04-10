6f3d6e7cd0e3180149311e69daff2856e4d179fc
rakhmanberd1n <154546932+rakhmanberd1n@users.noreply.github.com>
2024-04-10 11:12:58 +0500

    Update base.py

--- tsis 8/1/base.py
+++ tsis 8/1/base.py
@@ -16,7 +16,7 @@ paddleSpeed = 20
 paddle = pygame.Rect(W // 2 - paddleW // 2, H - paddleH - 30, paddleW, paddleH)
 
 
-#Ball
+
 ballRadius = 20
 ballSpeed = 6
 ball_rect = int(ballRadius * 2 ** 0.5)
@@ -24,7 +24,7 @@ ball = pygame.Rect(random.randrange(ball_rect, W - ball_rect), H // 2, ball_rect
 dx, dy = 1, -1
 
 
-#Game over
+
 font = pygame.font.SysFont('comicsansms', 40)
 text = font.render('Game Over', True, (255, 255, 255))
 textRect = text.get_rect()
@@ -42,7 +42,7 @@ while not done:
     pygame.draw.circle(screen, pygame.Color(250, 241, 157), ball.center, ballRadius)
     
     
-    #Paddle Control
+    
     key = pygame.key.get_pressed()
     if key[pygame.K_LEFT] and paddle.left > 0:
         paddle.left -= paddleSpeed
@@ -50,17 +50,17 @@ while not done:
         paddle.right += paddleSpeed
 
 
-    #Ball movement
+    
     ball.x += ballSpeed * dx
     ball.y += ballSpeed * dy
 
-    # #Collision left 
+    
     if ball.centerx < ballRadius or ball.centerx > W - ballRadius:
         dx = -dx
-    #Collision TOP
+    
     if ball.centery < ballRadius + 50: 
         dy = -dy
-    #Collision with paddle
+   
     if ball.colliderect(paddle) and dy > 0:
         dy = -dy
     if ball.y > H or ball.x > W:
